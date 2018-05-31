9316958c532241d9375eed0f07587b8cb3241676
DESKTOP-0H1RNH8\huozi <huozuyinshua@gmail.com>
2018-06-01 02:17:30 +0900

    save

---
+++ RTS/Assets/ParallaxEffect.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour {
+    public Transform rootOfObjects;
+    List<Transform> objectList;
+
+    float cameraClippingPlane;
+
+    public float movingSpeed = 1;
+    
+    void setObjectList(Transform father)
+    {
+        if (father.childCount == 0)
+        {
+            objectList.Add(father);
+            return;
+        }
+
+        for(int i = 0; i < father.childCount; i++)
+        {
+            setObjectList(father.GetChild(i));
+        }
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+        objectList = new List<Transform>();
+        setObjectList(rootOfObjects);
+        cameraClippingPlane = this.GetComponent<Camera>().farClipPlane;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       // GetComponent<Camera>().fieldOfView -= 0.01f;
+     //   Debug.Log(GetComponent<Camera>().fieldOfView);
+        foreach (Transform ob in objectList)
+        {
+           // float distance = Mathf.Abs(Vector3.Distance(ob.transform.position,this.transform.position));
+            //float moving = cameraClippingPlane / distance;
+            ob.Translate(this.transform.right * Time.deltaTime*(movingSpeed),Space.World);
+        }
+	}
+}
---
+++ RTS/Assets/ParticleControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleControl : MonoBehaviour {
+    public Transform particleObjs;
+    bool key = true;
+    // Use this for initialization
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (key)
+        {
+            particleObjs.GetComponent<ParticleSystem>().Play(true);
+
+
+            key = false;
+        }
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
